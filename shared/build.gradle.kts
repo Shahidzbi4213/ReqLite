593addc6a7d374ee759810a97d118651efd249db
Shahid Iqbal <shahid1.smartech@gmail.com>
2026-08-22 17:06:02 +0500

    Update android block and jvmTarget to JVM_21 in shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -9,13 +9,13 @@ plugins {
 }
 
 kotlin {
-    androidLibrary {
+    android {
         namespace = "com.jetbrains.kmpapp.shared"
         compileSdk = libs.versions.android.compileSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
 
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_11
+            jvmTarget = JvmTarget.JVM_21
         }
 
         androidResources {
